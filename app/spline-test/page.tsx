31fb5d1ee8f6ac43aab51b49db7a071a2492862a
Aidil Fitra <aidil.fitra@asi-asiapacific.com>
2025-10-30 13:29:36 +0700

    feat: add initial page component with Spline integration

---
+++ app/spline-test/page.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function Page() {
+  return (
+    <main>
+      <Spline scene="https://prod.spline.design/tlJWZ9E7cJQbfVyA/scene.splinecode" />
+    </main>
+  );
+}
